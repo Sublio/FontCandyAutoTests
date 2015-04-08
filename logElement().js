0b4aa3ca51a6ce1b53543121520a6ae530dfd0d6
Sublio <Sublio1260@gmail.com>
2015-04-08 14:05:01 +0600

    second commit

---
+++ logElement().js
@@ -0,0 +1,19 @@
+
+
+
+
+#import "Screen.js";
+#import "env.js";
+
+
+
+test("Проверка 1 ", function(){
+
+
+
+	target.delay(2);
+
+     target.delay(2);
+     window.scrollViews()[0].collectionViews()[0].cells()[0].tap();
+     window.buttons()["Skip"].logElement();
+})
